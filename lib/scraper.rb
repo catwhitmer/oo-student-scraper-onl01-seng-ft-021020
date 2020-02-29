0a086cb6784329889fee2a9f18decf29eb21b51a
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-29 00:47:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,31 +1,11 @@
 require 'open-uri'
 require 'pry'
 
-class Scraper
 
-  def self.scrape_index_page(index_url)
-    
-   doc = Nokogiri::HTML(open(index_url))
-    student = []
-    
-    student[:name] = doc.search()
-    student[:location] = doc.search()
-    student[:profile_url] = doc.search()
-    
-    student
-    
-  end
 
 
   def self.scrape_profile_page(profile_url)
-    doc = Nokogiri::HTML(open(profile_url))
-    student = {}
-    
-    student[:name] = doc.search(<h1 class="profile-name">Ryan Johnson</h1>).text
-    student[:location] = doc.search(<h2 class="profile-location">New York, NY</h2>).text
-    student[:profile_url] = doc.search(profile_url).text
-    
-    student
+
   end
 
 end
