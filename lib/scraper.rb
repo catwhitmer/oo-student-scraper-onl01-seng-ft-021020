b48972f00140fde88961f42c6877b8dc571c3015
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-29 00:54:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Scraper
 
-def self.scrape_index_page(index_url)
+  def self.scrape_index_page(index_url)
     page = Nokogiri::HTML(open(index_url))
     students = []
 
@@ -20,7 +20,28 @@ def self.scrape_index_page(index_url)
     students
    end
 
+  def self.scrape_profile_page(profile_url)
+      page = Nokogiri::HTML(open(profile_url))
+      student = {}
 
+      # student[:profile_quote] = page.css(".profile-quote")
+      # student[:bio] = page.css("div.description-holder p")
+      container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
+      container.each do |link|
+        if link.include?("twitter")
+          student[:twitter] = link
+        elsif link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("github")
+          student[:github] = link
+        elsif link.include?(".com")
+          student[:blog] = link
+        end
+      end
+      student[:profile_quote] = page.css(".profile-quote").text
+      student[:bio] = page.css("div.description-holder p").text
+      student
+  end
   
 
 end
