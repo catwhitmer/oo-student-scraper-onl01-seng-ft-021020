616705998f229353e454f9177dec4774fa7bac7b
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-29 00:34:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,17 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     
+   doc = Nokogiri::HTML(open(index_url))
+    student = {}
+    
+    student[:name] = doc.search()
+    student[:location] = doc.search()
+    student[:profile_url] = doc.search()
+    
+    student
+    
   end
+
 
   def self.scrape_profile_page(profile_url)
     
@@ -13,3 +23,4 @@ class Scraper
 
 end
 
+ 
